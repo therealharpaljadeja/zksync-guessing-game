a604894623e5b266da66f651c10244db8eb7e8ee
Harpalsinh Jadeja <jadejaharpal14@gmail.com>
2024-03-22 17:08:29 +0530

    fix: add `abi` to `frontend` folder

--- frontend/utils/constants.ts
+++ frontend/utils/constants.ts
@@ -3,4 +3,4 @@ export const NETWORK_ID = "0x12c";
 export const NETWORK_NAME = "zkSync Era Sepolia Testnet";
 export const GUESSING_GAME_CONTRACT = "0xdaE31a4519Cb0418a91c34f379c3fdc2096f3E20";
 export const GUESSING_GAME_CONTRACT_ABI =
-    require("../../artifacts-zk/contracts/GuessingGame.sol/GuessingGame.json").abi;
+    require("../abi/GuessingGame.sol/GuessingGame.json").abi;
